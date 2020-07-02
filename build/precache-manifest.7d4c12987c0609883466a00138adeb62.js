8acf1591b8aa91bfb7c262c1c6aefa496327a00b
Amir <amir@vansover.com>
2020-07-02 20:20:20 +0300

    react app in git

---
+++ build/precache-manifest.7d4c12987c0609883466a00138adeb62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80152043856be222094a2b943512e373",
+    "url": "/reactrep/index.html"
+  },
+  {
+    "revision": "5406a39269e1a0c65917",
+    "url": "/reactrep/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f3e49a1cf87bce57009",
+    "url": "/reactrep/static/js/2.83d1a170.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactrep/static/js/2.83d1a170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5406a39269e1a0c65917",
+    "url": "/reactrep/static/js/main.95d3c72b.chunk.js"
+  },
+  {
+    "revision": "749fdcb9b0e50d654116",
+    "url": "/reactrep/static/js/runtime-main.e81cf02b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactrep/static/media/logo.5d5d9eef.svg"
+  }
+]);
